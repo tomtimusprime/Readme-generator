1f9261e23fa70ce4f61eccbc82a4a6e98927da40
Tom Black <tom.black06007@gmail.com>
2020-04-29 18:55:22 -0700

    Added API call functionality.

--- index.js
+++ index.js
@@ -5,61 +5,100 @@ const axios = require("axios");
 
 // const readFileAsync = util.promisify(fs.readFile);
 const writeFileAsync = util.promisify(fs.writeFile);
+const axiosAsync = util.promisify(axios.get);
 
-// readFileAsync("animals.json", "utf8").then(function(data) {
-
-//   });
-
-  inquirer
+inquirer
     .prompt([
         {
             type: "input",
-            message: "Enter your full name",
-            name: "fullName"
+            message: "What's your Github username?",
+            name: "githubName"
         },
         {
             type: "input",
-            message: "Where do you currently live?",
-            name: "location"
+            message: "Project Title?",
+            name: "title"
         },
         {
             type: "input",
-            message: "What's your Linkedin URL?",
-            name: "linkedin"
+            message: "Enter a project description.",
+            name: "description"
         },
         {
             type: "input",
-            message: "Enter your Github Username",
-            name: "github"
+            message: "How will you install your program?",
+            name: "installation"
         },
         {
             type: "input",
-            message: "Write a quick Bio.",
-            name: "bio"
+            message: "How are you going to use this project?",
+            name: "usage"
+        },
+        {
+            type: "checkbox",
+            message: "What type of licensing are you using for this project?",
+            name: "licensing",
+            choices: [
+                "None",
+                "MIT",
+                "APACHE_2.0",
+                "GPL_3.0",
+                "BSD3",
+                "Other"
+            ]
+        },
+        {
+            type: "input",
+            message: "Authors?",
+            name: "authors"
+        },
+        {
+            type: "input",
+            message: "How can others contribute?",
+            name: "contributors"
+        },
+        {
+            type: "input",
+            message: "Tests?",
+            name: "tests"
+        },
+        {
+            type: "input",
+            message: "Questions?",
+            name: "questions"
+        },
+        {
+            type: "input",
+            message: "Where should the user report issues?",
+            name: "reporting"
         }
     ])
     .then((data) => {
-        
-        writeFileAsync("Readme.txt", 
-        `<!DOCTYPE html>
-        <html lang="en">
-        <head>
-            <meta charset="UTF-8">
-            <meta name="viewport" content="width=device-width, initial-scale=1.0">
-            <title>Document</title>
-        </head>
-        <body>
-            <h1>Full Name:${data.fullName} </h1>
-            <h1>Full Current Location: ${data.location}</h1>
-            <h1>Full LinkedIn profile: ${data.linkedin} </h1>
-            <h1>Full Github Link: ${data.github} </h1>
-            <p>Bio: ${data.bio}</p>
-        </body>
-        </html>`
-        , (err) => {
-            if(err) {
-                throw err;
-            }
-            console.log("Successfully created your HTML file.");
-        })
+
+        const queryUrl = `https://api.github.com/users/${username}/repos?per_page=100`;
+
+        axios.get(queryUrl).then(function (res) {
+            const repoNames = res.data.map(function (repo) {
+                return repo.name;
+            });
+
+            const repoNamesStr = repoNames.join("\n");
+
+            fs.writeFile("repos.txt", repoNamesStr, function (err) {
+                if (err) {
+                    throw err;
+                }
+
+                console.log(`Saved ${repoNames.length} repos`);
+            });
+        });
+
+        writeFileAsync("Readme.txt",
+            `# ${data.title}`
+            , (err) => {
+                if (err) {
+                    throw err;
+                }
+                console.log("Successfully created your HTML file.");
+            })
     })
